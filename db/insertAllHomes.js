5e201c69e20392cbf3930cab60a21c2fe09df45c
Muhammad Mosaad <mm458@njit.edu>
2018-12-20 10:28:55 -0500

    renamed parameter

--- db/insertAllHomes.js
+++ db/insertAllHomes.js
@@ -13,55 +13,55 @@ var insertAllHomes = () => {
   }
 };
 
-var generateHomeAttributes = (home, num, cities) => {
-  if (num < 25) {
-    return decorateLowTier(home, num, cities);
-  } else if (num < 50) {
-    return decorateMidTier(home, num, cities);
-  } else if (num < 75) {
-    return decorateHighTier(home, num, cities);
+var generateHomeAttributes = (home, id, cities) => {
+  if (id < 25) {
+    return decorateLowTier(home, id, cities);
+  } else if (id < 50) {
+    return decorateMidTier(home, id, cities);
+  } else if (id < 75) {
+    return decorateHighTier(home, id, cities);
   } else {
-    return decorateBallerTier(home, num, cities);
+    return decorateBallerTier(home, id, cities);
   }
 };
 
-var decorateLowTier = (home, num, cities) => {
+var decorateLowTier = (home, id, cities) => {
   home.address = faker.address.streetAddress();
   home.city = cities[getRandomNumber(0, 4)];
   home.price = getRandomNumber(250000, 500000);
   home.bedNum = 1;
   home.bathNum = 1;
-  home.imageUrl = `https://s3.amazonaws.com/hrnyc19apartmentimages/Images/${num}.jpg`;
+  home.imageUrl = `https://s3.amazonaws.com/hrnyc19apartmentimages/Images/${id}.jpg`;
   return home;
 };
 
-var decorateMidTier = (home, num, cities) => {
+var decorateMidTier = (home, id, cities) => {
   home.address = faker.address.streetAddress();
   home.city = cities[getRandomNumber(0, 4)];
   home.price = getRandomNumber(500001, 1000000);
   home.bedNum = getRandomNumber(1, 2);
   home.bathNum = getRandomNumber(1, home.bedNum);
-  home.imageUrl = `https://s3.amazonaws.com/hrnyc19apartmentimages/Images/${num}.jpg`;
+  home.imageUrl = `https://s3.amazonaws.com/hrnyc19apartmentimages/Images/${id}.jpg`;
   return home;
 };
 
-var decorateHighTier = (home, num, cities) => {
+var decorateHighTier = (home, id, cities) => {
   home.address = faker.address.streetAddress();
   home.city = cities[getRandomNumber(0, 4)];
   home.price = getRandomNumber(1000001, 3000000);
   home.bedNum = getRandomNumber(2, 4);
   home.bathNum = getRandomNumber(2, home.bedNum === 2 ? 2 : home.bedNum - 1);
-  home.imageUrl = `https://s3.amazonaws.com/hrnyc19apartmentimages/Images/${num}.jpg`;
+  home.imageUrl = `https://s3.amazonaws.com/hrnyc19apartmentimages/Images/${id}.jpg`;
   return home;
 };
 
-var decorateBallerTier = (home, num, cities) => {
+var decorateBallerTier = (home, id, cities) => {
   home.address = faker.address.streetAddress();
   home.city = cities[getRandomNumber(0, 1)];
   home.price = getRandomNumber(3000001, 20000000);
   home.bedNum = getRandomNumber(3, 7);
   home.bathNum = getRandomNumber(2, home.bedNum < 5 ? 3 : home.bedNum - 2);
-  home.imageUrl = `https://s3.amazonaws.com/hrnyc19apartmentimages/Images/${num}.jpg`;
+  home.imageUrl = `https://s3.amazonaws.com/hrnyc19apartmentimages/Images/${id}.jpg`;
   return home;
 };
 
