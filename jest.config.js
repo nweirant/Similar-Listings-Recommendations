522a04efd4d90edae4f937fa42c831712e87b4ec
Muhammad Mosaad <mm458@njit.edu>
2018-12-23 19:12:24 -0500

    cleaned up code

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,3 @@
 module.exports = {
   preset: "jest-puppeteer"
-
-  //   launch: {
-  //     dumpio: true,
-  //     headless: process.env.HEADLESS !== "false"
-  //   },
-  //   server: {
-  //     command: "node server.js",
-  //     port: 4444
-  //   }
 };
