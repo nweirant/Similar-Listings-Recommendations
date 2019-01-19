371a4332f936b184a3435939d18e7c6ce9ba0773
Talal <nweirant@gmail.com>
2019-01-18 19:19:37 -0500

    removed ORM, app performs better

--- db/index.js
+++ db/index.js
@@ -1,15 +1,38 @@
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
+//var mongo = require('mongodb').MongoClient;
+//mongoose.connect(`mongodb://talal:shadie12@54.175.166.47/homes`);
+//var url = `mongodb://localhost:27017/homes`;
+//
+// mongo.connect(url, (err, client) => {
+//   if (err) {
+//     console.error(err)
+//     return
+//   }
+//   //...
+//   console.log('Mongo connected to localhost');
+//   const db = client.db('homes');
+//   const collection = db.collection('homes');
+//   collection.find({homeId : { $in: similarlistings }} , (err,results) => {
+//     if (err) {
+//       console.log('error finding');
+//     } else {
+//       console.log('results',results);
+//
+//     }
+//   });
+// });
 
-mongoose.connect(`mongodb://talal:shadie12@54.175.166.47/homes`);
-const db = mongoose.connection;
 
-//mongoose.Promise = Promise;
-db.on("error", (err) => {
-  console.log("connection err");
-});
 
-db.once("open", () => {
-  console.log("MongoDB connected!");
-});
 
-module.exports = db;
+
+// //mongoose.Promise = Promise;
+// db.on("error", (err) => {
+//   console.log("connection err");
+// });
+//
+// db.once("open", () => {
+//   console.log("MongoDB connected!");
+// });
+
+//module.exports = collection;
--- server/helpers/homes.js
+++ server/helpers/homes.js
@@ -1,4 +1,5 @@
-const Home = require("../../db/homeModel.js");
+//const Home = require("../../db/homeModel.js");
+const db = require("../../db/index.js");
 const homeHelpers = require("./homesHelpers.js");
 
 const homes = {
@@ -15,14 +16,35 @@ const homes = {
   },
 
   getHomes: (id, similarlistings, callback) => {
-    console.log(similarlistings);
-      Home.find({ homeId: { $in: similarlistings } }, (err, data) => {
+    //console.log(similarlistings);
+    //console.log(db.count({}));
+    //   Home.find({ homeId: { $in: similarlistings } }, (err, data) => {
+    //   if (err) {
+    //     console.log(err);
+    //     callback(err);
+    //   } else {
+    //     callback(null, data);
+    //   }
+    // });
+    var mongo = require('mongodb').MongoClient;
+    var url = `mongodb://localhost:27017/homes`;
+    var similar = similarlistings;
+    mongo.connect(url, (err, client) => {
       if (err) {
-        console.log(err);
-        callback(err);
-      } else {
-        callback(null, data);
+        console.error('error connecting to mongo')
+        return
       }
+      console.log('Mongo connected to localhost');
+      const db = client.db('homes');
+      const collection = db.collection('homes');
+      collection.find({ homeId: { $in: similar } }).toArray(function(err,results) {
+        if (err) {
+          console.log('error finding similar homes');
+        } else {
+          //console.log(results);
+          callback(null, results);
+        }
+      });
     });
   }
 };
